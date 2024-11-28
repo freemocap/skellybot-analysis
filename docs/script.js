040e3572d58127014732232d09653d067cf429f5
jonmatthis <jonmatthis@gmail.com>
2024-11-28 10:43:41 -0500

    click to focus

--- docs/script.js
+++ docs/script.js
@@ -150,7 +150,24 @@ class GraphManager {
                 sprite.color = node.color;
                 sprite.textHeight = 8;
                 return sprite;
-            });
+            })
+            .onNodeClick(node => {
+                // Aim at node from outside it
+                const distance = 100;
+                const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+
+                const newPos = node.x || node.y || node.z
+                    ? { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }
+                    : { x: 0, y: 0, z: distance }; // special case if node is in (0,0,0)
+
+                this.graph.cameraPosition(
+                    newPos, // new position
+                    node, // lookAt ({ x, y, z })
+                    1000  // ms transition duration
+                );
+            }
+            )
+        ;
         this.graph.d3Force('link').distance(link => calculateLinkDistance(link, this.controls.controls));
     }
 }
